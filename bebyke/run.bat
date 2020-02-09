ead927f9ed97974f4cdb340b9835dfb0c412f5f1
NikolaiTTkachenko <51858131+NikolaiTTkachenko@users.noreply.github.com>
2020-02-09 16:56:09 +0200

    update run command

--- bebyke/run.bat
+++ bebyke/run.bat
@@ -1,1 +1,1 @@
- java "-classpath" "classes;lib/commons-cli-1.4.jar;lib/commons-lang3-3.8.1.jar;lib/commons-validator-1.4.0.jar;lib/opencsv-4.5.jar" ua.net.tnt.App -f c:/downloads/ecobike.txt -l 20
+ java "-classpath" "target/classes;target/lib/commons-cli-1.4.jar;target/lib/commons-lang3-3.8.1.jar;target/lib/commons-validator-1.4.0.jar;target/lib/opencsv-4.5.jar" ua.net.tnt.App -f target/ecobike.txt -l 20
